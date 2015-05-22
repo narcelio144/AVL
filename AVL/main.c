dc6b9aeca430057395ad220c2c43f90409addfe8
Narcélio Medeiros <narcelio@Air-de-Narcelio.home>
2015-05-22 08:51:43 -0300

    Initial Commit

---
+++ AVL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AVL
+//
+//  Created by Narcélio Medeiros on 22/05/15.
+//  Copyright (c) 2015 Narcélio Medeiros. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
